e348fdaaccb9933ad3a821b05f9b3a29b9aacb9c
Brady Wied <brady@wied.us>
2014-07-09 21:14:30 -0500

    Add shell resource/provider

---
+++ providers/apt_baseline.rb
@@ -0,0 +1,9 @@
+def whyrun_supported?
+  true
+end
+
+use_inline_resources
+
+action :install do
+  fail 'write the provider'
+end
---
+++ resources/apt_baseline.rb
@@ -0,0 +1,4 @@
+actions :install
+default_action :install
+
+attribute :csv_filename, :kind_of => String, :name_attribute => true

3d23338b80b12297e937ca18a2d9658f06560d99
Brady Wied <brady@wied.us>
2014-08-15 18:16:34 -0500

    Add test for figuring out the CSV before calling the provider (easier for items looking at the resource collection)

--- test/unit/spec/lwrp_csv_to_apt_resources_spec.rb
+++ test/unit/spec/lwrp_csv_to_apt_resources_spec.rb
@@ -31,6 +31,34 @@ describe 'bsw_apt_baseline::lwrp:apt_baseline' do
     'csv_to_apt_resources'
   end
 
+  it 'loads the packages in the CSV in the resource' do
+    # arrange
+    lwrp = <<-EOF
+          bsw_apt_baseline_csv_to_apt_resources 'test1.csv'
+    EOF
+    setup_command 'bash ii
+            openssl ii
+        '
+    create_temp_cookbook lwrp
+    csv_path = File.join cookbook_path, 'files', 'default', 'test1.csv'
+    FileUtils.mkdir_p File.dirname(csv_path)
+    CSV.open csv_path, 'w' do |csv|
+      csv << ['package', 'repository', 'version']
+      csv << ['bash', 'amd64/trusty-security', '1.4.2']
+      csv << ['openssl', 'amd64/trusty-security', '1.5.2']
+    end
+
+    # act
+    temp_lwrp_recipe lwrp
+
+    # assert
+    resource = @chef_run.find_resource('bsw_apt_baseline_csv_to_apt_resources', 'test1.csv')
+    expect(resource.packages).to eq([
+                                        {:name => 'bash', :version => '1.4.2'},
+                                        {:name => 'openssl', :version => '1.5.2'},
+                                    ])
+  end
+
   it 'parses the CSV and creates resources appropriately when both are installed' do
     # arrange
     lwrp = <<-EOF

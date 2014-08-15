9a5f55c5fafc3df3384d77737dc93df48e754f30
Brady Wied <brady@wied.us>
2014-08-15 18:37:28 -0500

    Tweak the tests to make sure we don't run apt-get upgrade for packages that haven't changed versions

--- test/unit/spec/lwrp_csv_to_apt_resources_spec.rb
+++ test/unit/spec/lwrp_csv_to_apt_resources_spec.rb
@@ -16,7 +16,7 @@ describe 'bsw_apt_baseline::lwrp:apt_baseline' do
   def setup_command(output)
     @shell_out = double()
     allow(Mixlib::ShellOut).to receive(:new)
-                               .with("dpkg-query -W -f='${binary:Package} ${db:Status-Abbrev}\\n'")
+                               .with("dpkg-query -W -f='${binary:Package} ${db:Status-Abbrev} ${Version}\\n'")
                                .and_return(@shell_out)
     allow(@shell_out).to receive(:live_stream=)
     allow(@shell_out).to receive(:run_command)
@@ -31,13 +31,13 @@ describe 'bsw_apt_baseline::lwrp:apt_baseline' do
     'csv_to_apt_resources'
   end
 
-  it 'loads the packages in the CSV in the resource' do
+  it 'parses the CSV and loads the packages in the resource' do
     # arrange
     lwrp = <<-EOF
           bsw_apt_baseline_csv_to_apt_resources 'test1.csv'
     EOF
-    setup_command 'bash ii
-            openssl ii
+    setup_command 'bash ii  1.4.2
+            openssl ii  1.5.2
         '
     create_temp_cookbook lwrp
     csv_path = File.join cookbook_path, 'files', 'default', 'test1.csv'
@@ -59,14 +59,62 @@ describe 'bsw_apt_baseline::lwrp:apt_baseline' do
                                     ])
   end
 
-  it 'parses the CSV and creates resources appropriately when both are installed' do
+  it 'does not upgrade if both packages are already installed' do
     # arrange
     lwrp = <<-EOF
-      bsw_apt_baseline_csv_to_apt_resources 'test1.csv'
+        bsw_apt_baseline_csv_to_apt_resources 'test1.csv'
     EOF
-    setup_command 'bash ii
-        openssl ii
-    '
+    setup_command 'bash ii  1.4.2
+          openssl ii  1.5.2
+      '
+    create_temp_cookbook lwrp
+    csv_path = File.join cookbook_path, 'files', 'default', 'test1.csv'
+    FileUtils.mkdir_p File.dirname(csv_path)
+    CSV.open csv_path, 'w' do |csv|
+      csv << ['package', 'repository', 'version']
+      csv << ['bash', 'amd64/trusty-security', '1.4.2']
+      csv << ['openssl', 'amd64/trusty-security', '1.5.2']
+    end
+
+    # act
+    temp_lwrp_recipe lwrp
+
+    # assert
+    expect(@chef_run).to_not run_execute('sudo apt-get -q -y upgrade bash=1.4.2 openssl=1.5.2')
+  end
+
+  it 'upgrades 1 of the packages if its behind' do
+    # arrange
+    lwrp = <<-EOF
+        bsw_apt_baseline_csv_to_apt_resources 'test1.csv'
+    EOF
+    setup_command 'bash ii  1.4.2
+          openssl ii  1.4.0
+      '
+    create_temp_cookbook lwrp
+    csv_path = File.join cookbook_path, 'files', 'default', 'test1.csv'
+    FileUtils.mkdir_p File.dirname(csv_path)
+    CSV.open csv_path, 'w' do |csv|
+      csv << ['package', 'repository', 'version']
+      csv << ['bash', 'amd64/trusty-security', '1.4.2']
+      csv << ['openssl', 'amd64/trusty-security', '1.5.2']
+    end
+
+    # act
+    temp_lwrp_recipe lwrp
+
+    # assert
+    expect(@chef_run).to run_execute('sudo apt-get -q -y upgrade openssl=1.5.2')
+  end
+
+  it 'upgrades both of the packages if they are both behind' do
+    # arrange
+    lwrp = <<-EOF
+          bsw_apt_baseline_csv_to_apt_resources 'test1.csv'
+    EOF
+    setup_command 'bash ii  1.4.0
+            openssl ii  1.4.0
+        '
     create_temp_cookbook lwrp
     csv_path = File.join cookbook_path, 'files', 'default', 'test1.csv'
     FileUtils.mkdir_p File.dirname(csv_path)
@@ -83,12 +131,12 @@ describe 'bsw_apt_baseline::lwrp:apt_baseline' do
     expect(@chef_run).to run_execute('sudo apt-get -q -y upgrade bash=1.4.2 openssl=1.5.2')
   end
 
-  it 'parses the CSV and creates resources appropriately when only 1 is installed' do
+  it 'upgrades appropriately when only 1 is installed' do
     # arrange
     lwrp = <<-EOF
         bsw_apt_baseline_csv_to_apt_resources 'test1.csv'
     EOF
-    setup_command 'openssl ii
+    setup_command 'openssl ii  1.4.0
         '
     create_temp_cookbook lwrp
     csv_path = File.join cookbook_path, 'files', 'default', 'test1.csv'

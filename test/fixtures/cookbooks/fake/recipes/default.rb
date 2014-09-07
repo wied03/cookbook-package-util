6c5430d5e03eb27d60cc3e1c2cc2fe3fb92a3e26
Brady Wied <brady@wied.us>
2014-09-07 17:08:18 -0500

    Added test kitchen tests

---
+++ test/fixtures/cookbooks/fake/recipes/default.rb
@@ -0,0 +1,2 @@
+file_name = node['platform_family'] == 'rhel' ? 'centos.csv' : 'ubuntu.csv'
+#bsw_package_util_csv_to_package_resources file_name
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -2,6 +2,13 @@
 
 require_relative 'spec_helper'
 
-describe 'default' do
-  it { pending 'write some tests' }
+describe package('openssl') do
+  expected_version = case os[:family]
+                       when 'RedHat'
+                         '1.0.1e-34.el7_0.4'
+                       when 'Ubuntu'
+                         '1.0.1f-1ubuntu2.5'
+                     end
+
+  it { should be_installed.with_version(expected_version) }
 end

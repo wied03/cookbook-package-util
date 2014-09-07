42a5adc96839f6e8a5d4e26e4e4d71d3b80c8866
Brady Wied <brady@wied.us>
2014-09-07 17:17:14 -0500

    Got test working for Ubuntu

--- test/fixtures/cookbooks/fake/recipes/default.rb
+++ test/fixtures/cookbooks/fake/recipes/default.rb
@@ -1,2 +1,7 @@
-file_name = node['platform_family'] == 'rhel' ? 'centos.csv' : 'ubuntu.csv'
-#bsw_package_util_csv_to_package_resources file_name
+platform_family = node['platform_family']
+file_name = platform_family == 'rhel' ? 'centos.csv' : 'ubuntu.csv'
+if platform_family == 'debian'
+  include_recipe 'apt::default'
+end
+
+bsw_package_util_csv_to_package_resources file_name

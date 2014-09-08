91965bba41410c0edfb2096012f67c4666c256e2
Brady Wied <brady@wied.us>
2014-09-08 16:55:25 -0500

    Fix test OS family

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,7 +4,7 @@ require_relative 'spec_helper'
 
 describe package('openssl') do
   expected_version = case os[:family]
-                       when 'RedHat'
+                       when 'RedHat7'
                          '1.0.1e-34.el7_0.4'
                        when 'Ubuntu'
                          '1.0.1f-1ubuntu2.5'
@@ -13,7 +13,7 @@ describe package('openssl') do
   it { should be_installed.with_version(expected_version) }
 end
 
-if os[:family] == 'RedHat'
+if os[:family] == 'RedHat7'
   describe package('postgresql93-libs') do
     it { should be_installed }
   end
